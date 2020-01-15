437fd8abc0400ddb343213a0adca3ca43bfcd10f
jjcabral <juan.cabral@gmail.com>
2020-01-14 19:37:23 -0600

    Empresas Fix movil

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items:1,
+      lazyLoad:true,
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:4000,
+      margin:0
+    });
+    $(".owl-carousel-2").owlCarousel({
+      items:1,
+      lazyLoad:true,
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:4000,
+      margin:0
+    });
+  });
+  
