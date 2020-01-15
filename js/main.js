7d8de12d2a59a146cb25185d37d538fd99c255fe
jjcabral <juan.cabral@gmail.com>
2020-01-15 07:59:23 -0600

    Add header new to empresas

--- js/main.js
+++ js/main.js
@@ -19,3 +19,16 @@ $(document).ready(function(){
     });
   });
   
+
+  $(function() {
+    var header = $(".header");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            header.removeClass('header').addClass("header-alt");
+        } else {
+            header.removeClass("header-alt").addClass('header');
+        }
+    });
+});
