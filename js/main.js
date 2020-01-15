1e1f5def2239e2729b01374f9339b5f967ade1b8
jjcabral <juan.cabral@gmail.com>
2020-01-15 07:55:01 -0600

    Add class on header at scroll and change Sliders

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
       items:1,
       lazyLoad:true,
+      nav: true,
       loop:true,
       autoplay:true,
       autoplayTimeout:4000,
@@ -10,6 +11,7 @@ $(document).ready(function(){
     $(".owl-carousel-2").owlCarousel({
       items:1,
       lazyLoad:true,
+      nav: true,
       loop:true,
       autoplay:true,
       autoplayTimeout:4000,
