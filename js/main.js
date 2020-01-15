0a30d4dc1d0948310d499e4157c012969da080ba
jjcabral <juan.cabral@gmail.com>
2020-01-15 08:52:02 -0600

    Fix botones slider empresas

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      items:1,
-      lazyLoad:true,
-      nav: true,
-      loop:true,
-      autoplay:true,
-      autoplayTimeout:4000,
-      margin:0,
-      nav: true
-    });
-    $(".owl-carousel-2").owlCarousel({
-      items:1,
-      lazyLoad:true,
-      nav: true,
-      loop:true,
-      autoplay:true,
-      autoplayTimeout:4000,
-      margin:0,
-      // nav: true
-    });
-  });
-  
+
 
   $(function() {
     var header = $(".header");
